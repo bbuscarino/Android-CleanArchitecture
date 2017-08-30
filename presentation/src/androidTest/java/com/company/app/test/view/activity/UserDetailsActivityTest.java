24b4575f829d90c83af1417261362a33cad43597
Ben Buscarino <buscari3@msu.edu>
2017-08-30 14:14:08 -0400

    Fixed instrumented tests

--- presentation/src/androidTest/java/com/company/app/test/view/activity/UserDetailsActivityTest.java
+++ presentation/src/androidTest/java/com/company/app/test/view/activity/UserDetailsActivityTest.java
@@ -16,10 +16,15 @@
 package com.company.app.test.view.activity;
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.test.ActivityInstrumentationTestCase2;
-import com.fernandocejas.android10.sample.presentation.R;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
 import com.company.app.presentation.view.activity.UserDetailsActivity;
+import com.fernandocejas.android10.sample.presentation.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -31,38 +36,31 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 
-public class UserDetailsActivityTest extends ActivityInstrumentationTestCase2<UserDetailsActivity> {
+@RunWith(AndroidJUnit4.class)
+public class UserDetailsActivityTest {
 
-  private static final int FAKE_USER_ID = 10;
+  @Rule
+  public ActivityTestRule<UserDetailsActivity> mUserDetailsActivityRule
+          = new ActivityTestRule<>(UserDetailsActivity.class);
 
-  private UserDetailsActivity userDetailsActivity;
-
-  public UserDetailsActivityTest() {
-    super(UserDetailsActivity.class);
-  }
-
-  @Override protected void setUp() throws Exception {
-    super.setUp();
-    this.setActivityIntent(createTargetIntent());
-    this.userDetailsActivity = getActivity();
-  }
-
-  @Override protected void tearDown() throws Exception {
-    super.tearDown();
-  }
-
+  @Test
   public void testContainsUserDetailsFragment() {
+    UserDetailsActivity activity = mUserDetailsActivityRule.getActivity();
     Fragment userDetailsFragment =
-        userDetailsActivity.getFragmentManager().findFragmentById(R.id.fragmentContainer);
+            activity.getFragmentManager().findFragmentById(R.id.fragmentContainer);
     assertThat(userDetailsFragment, is(notNullValue()));
   }
 
+  @Test
   public void testContainsProperTitle() {
-    String actualTitle = this.userDetailsActivity.getTitle().toString().trim();
+    UserDetailsActivity activity = mUserDetailsActivityRule.getActivity();
+
+    String actualTitle = activity.getTitle().toString().trim();
 
     assertThat(actualTitle, is("User Details"));
   }
 
+  @Test
   public void testLoadUserHappyCaseViews() {
     onView(withId(R.id.rl_retry)).check(matches(not(isDisplayed())));
     onView(withId(R.id.rl_progress)).check(matches(not(isDisplayed())));
@@ -72,16 +70,10 @@ public class UserDetailsActivityTest extends ActivityInstrumentationTestCase2<Us
     onView(withId(R.id.tv_description)).check(matches(isDisplayed()));
   }
 
+  @Test
   public void testLoadUserHappyCaseData() {
     onView(withId(R.id.tv_fullname)).check(matches(withText("John Sanchez")));
     onView(withId(R.id.tv_email)).check(matches(withText("dmedina@katz.edu")));
     onView(withId(R.id.tv_followers)).check(matches(withText("4523")));
-  }
-
-  private Intent createTargetIntent() {
-    Intent intentLaunchActivity =
-        UserDetailsActivity.getCallingIntent(getInstrumentation().getTargetContext(), FAKE_USER_ID);
-
-    return intentLaunchActivity;
   }
 }
